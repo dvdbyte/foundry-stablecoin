585e5d8e84874c042d26965d52a9e222de20c39e
dvd <dvdbyte1@gmail.com>
2024-04-17 16:21:24 +0100

    Deposi tCollateralAndMint Function Added

--- src/DCSEngine.sol
+++ src/DCSEngine.sol
@@ -2,40 +2,105 @@
 
 pragma solidity ^0.8.18;
 
+import {DecentralizedStableCoin} from "./DecentralizedStableCoin.sol";
+import {ReentrancyGuard} from "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
 /**
  * @title DSCEngine
  * @author dvdbyte
- * 
- * 
+ *
+ *
  * The system is designed to be as minimal as possible, and have the tokens maintain a 1 token == 1$ peg
  * This stablecoin has the following properties:
  * -Exogenous Collateral
  * -Dollar Pegged
  * -Algorithmically Stable
- * 
+ *
  * It is similar to DAI if DAI had no governance, no fees, and was only backed by WETH and WBTC
- * 
+ *
  * Our DSC System should always be "Overcollateralized" At no point, should the value of all collateral <= the $ backed value of all the DSC.
- * 
+ *
  * @notice This contract is the core of the system. It handles all the logic for minting and redeeming DSC, as well as depositing & withdrawing collateral.
  * @notice this contract is VERY loosely based on the MakerDao DSS (DAI) system
  */
 
-contract DSCEngine {
-  function depositCollateralAndMintDsc() external {}
+contract DSCEngine is ReentrancyGuard {
+    error DCCEngine__NeedsMoreThanZero();
+    error DCEngine__TokenAddressesAndPriceFeedAddressesMustBeSameLength();
+    error DCCEngine__NotAllowedToken();
+    error DSCEngine__TransferFailed();
 
-  function depositCollateral() external {}
+    mapping(address token => address priceFeeds) private s_priceFeeds;
+    mapping(address user => mapping(address token => uint256 amount))
+        private s_collateralDeposited;
+    DecentralizedStableCoin private immutable i_dsc;
 
-  function redeemCollateralForDsc() external {}
+    event CollateralDeposited(address indexed user, address indexed token,  uint256 indexed amount);
 
-  function redeemCollateral() external {}
+    constructor(
+        address[] memory tokenAddresses,
+        address[] memory priceFeedAddresses,
+        address dscAddress
+    ) {
+        if (tokenAddresses.length != priceFeedAddresses.length) {
+            revert DCEngine__TokenAddressesAndPriceFeedAddressesMustBeSameLength();
+        }
+        for (uint256 i = 0; i < tokenAddresses.length; i++) {
+            s_priceFeeds[tokenAddresses[i]] = priceFeedAddresses[i];
+        }
+        i_dsc = DecentralizedStableCoin(dscAddress);
+    }
 
-  funtion mintDsc() external {}
+    modifier moreThanZero(uint256 amount) {
+        if (amount == 0) {
+            revert DCCEngine__NeedsMoreThanZero();
+        }
+        _;
+    }
 
-  function burnDsc() external {}
+    modifier isAllowedToken(address token) {
+        if (s_priceFeeds[token] == address(0)) {
+            revert DCCEngine__NotAllowedToken();
+        }
+        _;
+    }
 
-  function liquidate() external {}
+    /*
+     * @param tokenCollateralAddress
+     * @param amountCollateral
+     */
 
-  function getHealthFactor() external view {}
+    function depositCollateralAndMintDsc(
+        address tokenCollateralAddress,
+        uint256 amountCollateral
+    )
+        external
+        moreThanZero(amountCollateral)
+        isAllowedToken(tokenCollateralAddress)
+        nonReentrant
+    {
+        s_collateralDeposited[msg.sender][tokenCollateralAddress] += amountCollateral;
+        emit CollateralDeposited(msg.sender, tokenCollateralAddress, amountCollateral);
+        bool succes= IERC20(tokenCollateralAddress).transferFrom(msg.sender, address(this), amountCollateral);
+        if (!succes) {
+          revert DSCEngine__TransferFailed();
+        }
 
+
+    }
+
+    function depositCollateral() external {}
+
+    function redeemCollateralForDsc() external {}
+
+    function redeemCollateral() external {}
+
+    function mintDsc() external {}
+
+    function burnDsc() external {}
+
+    function liquidate() external {}
+
+    function getHealthFactor() external view {}
 }
